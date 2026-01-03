c79d3294a398d4eb287292de843a72fc26d6e948
jimm-8 <peacti10@gmail.com>
2026-01-03 23:12:02 +0800

    (frontend) odr ui no backend yet

--- frontend/src/components/common/odr_tabs.jsx
+++ frontend/src/components/common/odr_tabs.jsx
@@ -1,8 +1,19 @@
 import Tab from "react-bootstrap/Tab";
 import Tabs from "react-bootstrap/Tabs";
-import { FaRegFile, FaSearch } from "react-icons/fa";
+import {
+  FaRegFile,
+  FaSearch,
+  FaRegClock,
+  FaRegEnvelope,
+  FaBullhorn,
+  FaInfoCircle,
+} from "react-icons/fa";
+import CARDBG from "../../assets/card_bg.png";
 
 const OdrTabs = () => {
+  const currentHour = new Date().getHours();
+  const isOpenHours = currentHour >= 8 && currentHour < 17;
+
   return (
     <>
       <Tabs
@@ -13,23 +24,175 @@ const OdrTabs = () => {
         <Tab
           eventKey="new_request"
           title={
-            <span className="flex items-center gap-2 whitespace-nowrap">
+            <span className="flex items-center text-[#28a745] gap-2 whitespace-nowrap">
               <FaRegFile /> New Request
             </span>
           }
         >
-          <p>Tab content for New Request</p>
+          <div>
+            {isOpenHours ? (
+              <p>Content is available!</p>
+            ) : (
+              <div className=" bg-[#fff3cd] p-3 m-5 rounded-md border-2 border-[#ffeeba] flex justify-center items-center">
+                <h1 className="text-[#856404] text-4xl">CLOSED</h1>
+              </div>
+            )}
+            <div className="border border-gray-400 m-5 p-3 rounded-md -translate-y-8">
+              <p className="flex items-center gap-2 text-xl p-2 text-gray-500 font-medium">
+                <FaRegClock className="text-gray-500" />
+                Office Hours
+              </p>
+              <hr />
+              <p className="text-xl mt-3 ml-5">Monday to Friday</p>
+              <p className="text-xl ml-5">8:00 AM to 5:00 PM</p>
+            </div>
+          </div>
         </Tab>
 
         <Tab
           eventKey="request_tracker"
           title={
-            <span className="flex items-center gap-2 whitespace-nowrap">
+            <span className="flex items-center text-[#17a2b8] gap-2 whitespace-nowrap">
               <FaSearch /> Request Tracker
             </span>
           }
         >
-          <p>Tab content for Request Tracker</p>
+          <div className="grid grid-cols-3 gap-3 p-1">
+            {/* office hours */}
+            <div
+              style={{ backgroundImage: `url(${CARDBG})` }}
+              className="border border-gray-400 rounded-md p-3 h-[260px] bg-contain bg-bottom bg-no-repeat"
+            >
+              <p className="flex items-center gap-2 text-xl p-2 text-gray-500 font-medium">
+                <FaRegClock className="text-gray-500" />
+                Office Hours
+              </p>
+              <hr />
+              <p className="text-lg mt-3 ml-5">Monday to Friday</p>
+              <p className="text-lg ml-5">8:00 AM to 5:00 PM</p>
+            </div>
+
+            {/* contact us */}
+            <div
+              style={{ backgroundImage: `url(${CARDBG})` }}
+              className="border border-gray-400 rounded-md p-3  bg-contain bg-bottom bg-no-repeat"
+            >
+              <p className="flex items-center gap-2 text-xl p-2 text-gray-500 font-medium">
+                <FaRegEnvelope className="text-gray-500" />
+                Contact Us
+              </p>
+              <hr />
+              <p className="text-sm ml-5 mt-3">
+                <a
+                  href="mailto:registrar.pb@g.batstate-u.edu.ph"
+                  className="hover:underline"
+                >
+                  registrar.pb@g.batstate-u.edu.ph
+                </a>
+              </p>
+
+              <p className="text-sm ml-5">
+                <a
+                  href="mailto:registrar.alangilan@g.batstate-u.edu.ph"
+                  className="hover:underline"
+                >
+                  registrar.alangilan@g.batstate-u.edu.ph
+                </a>
+              </p>
+
+              <p className="text-sm ml-5">
+                <a
+                  href="mailto:registrar.lipa@g.batstate-u.edu.ph"
+                  className="hover:underline"
+                >
+                  registrar.lipa@g.batstate-u.edu.ph
+                </a>
+              </p>
+
+              <p className="text-sm ml-5">
+                <a
+                  href="mailto:registrar.nasugbu@g.batstate-u.edu.ph"
+                  className="hover:underline"
+                >
+                  registrar.nasugbu@g.batstate-u.edu.ph
+                </a>
+              </p>
+
+              <p className="text-sm ml-5">
+                <a
+                  href="mailto:registrar.malvar@g.batstate-u.edu.ph"
+                  className="hover:underline"
+                >
+                  registrar.malvar@g.batstate-u.edu.ph
+                </a>
+              </p>
+            </div>
+
+            {/* advisory */}
+            <div
+              style={{ backgroundImage: `url(${CARDBG})` }}
+              className="border border-gray-400 rounded-md p-3  bg-contain bg-bottom bg-no-repeat"
+            >
+              <p className="flex items-center gap-2 text-xl p-2 text-gray-500 font-medium">
+                <FaBullhorn className="text-gray-500" />
+                Advisory
+              </p>
+              <hr />
+              <p className="text-sm mt-3">
+                Your document request PIN (4 digit) together with the REFERENCE
+                NO. is now required for tracking. You can find the PIN on the
+                confirmation email sent to you. You can ask the assistance of
+                the Registration Services office if you accidentally deleted the
+                email.
+              </p>
+            </div>
+          </div>
+          <div className="max-w-full border-[#17a2b8] border-2 rounded-md bg-[#d1ecf1] p-3 m-1">
+            <p className="flex items-start text-[#0c5460] gap-2">
+              <FaInfoCircle className="mt-1 text-xl" />
+              <span>
+                To track the status of your document request, please enter the{" "}
+                <span className="font-bold">REFERENCE NUMBER</span> and{" "}
+                <span className="font-bold">PIN</span> below.
+              </span>
+            </p>
+          </div>
+
+          <div className="flex  w-full max-w-full translate-y-4 p-1">
+            <div className="flex items-center flex-1 border border-gray-900 rounded-md bg-white">
+              {/* Reference Number Section */}
+              <div className="flex border-r border-gray-300">
+                <label className="px-3 py-2 font-medium text-sm text-gray-600 bg-gray-100 border-r border-gray-300 whitespace-nowrap">
+                  Reference Number
+                </label>
+                <input
+                  type="text"
+                  placeholder="Reference No."
+                  className="px-3 py-2 text-sm outline-none w-[21.5rem]"
+                />
+              </div>
+
+              {/* PIN Section */}
+              <div className="flex items-center border-r border-gray-300">
+                <label className="px-3 py-2 font-medium text-sm text-gray-600 bg-gray-100 border-r border-gray-300">
+                  PIN
+                </label>
+                <input
+                  type="password"
+                  placeholder="4 digit PIN"
+                  maxLength={4}
+                  inputMode="numeric"
+                  className="px-3 py-2 text-sm outline-none w-[21.3rem]"
+                />
+              </div>
+
+              {/* Track Button */}
+              <button className="flex items-center font-medium gap-2 px-3 py-2 text-sm text-gray-600 bg-gray-100 hover:bg-gray-200 transition-colors">
+                <FaSearch size={16} />
+                Track
+              </button>
+            </div>
+          </div>
         </Tab>
       </Tabs>
     </>
--- frontend/src/pages/odr/odr_requests.jsx
+++ frontend/src/pages/odr/odr_requests.jsx
@@ -4,14 +4,14 @@ import OdrTabs from "../../components/common/odr_tabs";
 
 const OdrRequests = () => {
   return (
-    <div className="bg-[#eeee] w-screen h-screen flex justify-center p-10">
+    <div className="bg-[#eeee] min-h-screen w-full flex justify-center overflow-x-hidden">
       <div className="flex flex-col items-center">
         <img
           src={ODR}
           alt="ODR"
-          className="w-2/3 h-auto block shadow-md shadow-black/20"
+          className="w-2/3 h-auto block shadow-lg shadow-black/80"
         />
-        <div className="w-2/3 h-full bg-white p-1 py-3">
+        <div className="w-2/3 h-full bg-white p-1 py-3 shadow-lg shadow-black/80">
           <OdrTabs />
         </div>
       </div>
