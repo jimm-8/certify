f45cf757446057e4dd573dff9bf0b765f7fc6748
jimm-8 <peacti10@gmail.com>
2026-01-07 15:09:07 +0800

    (janine) setup for janine laptop

--- backend/venv/Scripts/activate.bat
+++ backend/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\User\Desktop\CODING PROJECTS\3. Certify\certify\backend\venv
+set VIRTUAL_ENV=C:\Users\Princess Janine\Desktop\THESIS\certify\backend\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
